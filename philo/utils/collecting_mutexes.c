8ea674842a9b9e81529d8ef09944fa536195abb2
Salah Eddine Moumni <smoumni@e3r7p15.1337.ma>
2024-01-27 18:09:18 +0100

    initial

--- philo/utils/collecting_mutexes.c
+++ philo/utils/collecting_mutexes.c
@@ -6,7 +6,7 @@
 /*   By: smoumni <smoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 22:35:16 by smoumni           #+#    #+#             */
-/*   Updated: 2024/01/26 22:35:43 by smoumni          ###   ########.fr       */
+/*   Updated: 2024/01/27 18:06:56 by smoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	collecting_mutexes(t_fork *forks, int num_of_forks, t_eve *eve)
 		if (pthread_mutex_destroy(&(forks[i].fork)))
 			return ;
 	i = -1;
+	pthread_mutex_destroy(eve->print_mutex);
 	while (eve && ++i < num_of_forks)
 	{
 		pthread_mutex_destroy(&(eve[i].last_meal_mutex));
